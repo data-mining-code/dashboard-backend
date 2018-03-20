72e297b6c45310ea567fd03b860376fd1903c813
8BitJonny <timon.christiansen@code.berlin>
2018-03-20 12:27:51 +0100

    Added functions to aggregate

--- UserClass.py
+++ UserClass.py
@@ -4,11 +4,12 @@ from textblob import TextBlob
 class User(object):
     """ classifies the attributes of the user and gets the overall sentiment of the users requests """
     def __init__(self, session, timestamp):
-
         self.log_list = [Request(session['chat'][log]) for log in session['chat']]
-        print(self.log_list)
         self.sessionid = timestamp
-        self.session = session
+        self.usertype = session['user_type']
+        self.questions_asked = session['questions_asked']
+        self.shops_asked = session['shops_asked']
+        self.products_asked = session['products_asked']
 
     def get_sentiment_overall(self):
         request = [req.get_sentiment() for req in self.log_list]
@@ -20,8 +21,28 @@ class User(object):
             mean_sen = reduce((lambda x,y: x + y), sen_array) / len(sen_array)
             return mean_sen
         else:
-            return sen_array
+            return sen_array[0]
+    
+    def get_questions_asked(self):
+        # Get every question asked but dont just one time
+        self.questions_asked = []
+        all_questions_asked = [req.query for req in self.log_list]
+        for query in all_questions_asked:
+            if query not in self.questions_asked:
+                self.questions_asked.append(query)
 
+    def get_shops_asked(self):
+        # Get every shop that was asked about but just one time
+        all_shops_asked = [req.location for req in self.log_list]
+        self.shops_asked = set(all_shops_asked)
+        if '' in self.shops_asked: self.shops_asked.remove('')
+
+    def get_products_asked(self):
+        # Get every product that was asked about but just one time
+        all_products_asked = [req.product for req in self.log_list]
+        self.products_asked = set(all_products_asked)
+        print (self.products_asked)
+        if '' in self.products_asked: self.products_asked.remove('')
 
 class Request(object):
 
@@ -31,7 +52,7 @@ class Request(object):
         self.client = log['request']['query']['client']
         self.location = log['request']['query']['location']
         self.notmatched = log['request']['query']['notmatched']
-        self.productid = log['request']['query']['productid']
+        self.product= log['request']['query']['product']
         self.product_key_words = log['request']['query']['product_key_words']
         self.question_key_words = log['request']['query']['question_key_words']
         self.question_words = log['request']['query']['question_words']
--- app.py
+++ app.py
@@ -28,36 +28,65 @@ auth = firebase.auth()
 user = auth.sign_in_with_email_and_password('metrodatamining@code.berlin', 'test123')
 
 def start():
-  pass
-  # get_chatlog
-  # get_usertypes
-  # get_userneeds
-  # save_with_analyzed=True
+    pass
+    # get_chatlog
+    # get_usertypes
+    # get_userneeds
+    # save_with_analyzed=True
 
+
+## Get new Chatlogs, update the local dic and aggregate the new ones
 def get_chatlog_stream(message):
     if message['path'] == '/':
         dataobject = message['data'] 
         for sess in dataobject:
             session_dic[sess] = User(dataobject[sess], sess)
-        print (session_dic)
+            if session_dic[sess].usertype == "":
+                aggregateData(session_dic[sess])
     else:
         sessid = message['path'].split('/')[1]
         new_log = dict(db.child("logs").child(sessid).get().val())
         session_dic[sessid] = User(new_log,sessid)
+        aggregateData(session_dic[sessid])
 
-        
+def aggregateData(UserObj):
+    UserObj.get_sentiment_overall()
+    UserObj.get_questions_asked()
+    UserObj.get_shops_asked()
+    UserObj.get_products_asked()
+
+## These Methods are called when the Call from Frontend comes        
 def  get_usertypes():
-  # classify each user into one of the three user types
-  # and create a list out of them
-  pass
+    # classify each user into one of the three user types
+    # and create a list out of them
+    pass
 
 def get_userneeds():
-  # classify each request's intent
-  # and also create a list out of them
-  pass
+    # classify each request's intent
+    # and also create a list out of them
+    pass
 
-def get_all_():
-  pass
+def top_questions_asked():
+    # Get the questions that were most often asked
+    pass
+
+def top_shops_asked():
+    # Get the shops that were most often asked
+    pass
+
+def top_products_asked():
+    # Get the products that were most often asked
+    product_resp = {}
+    # Get the products that were most often asked
+    all_products_asked_for = []
+    for sess in sessdic:
+        for prdct in sessdic[sess].products_asked:
+            all_products_asked_for.append(prdct)
+    counted = Counter(all_products_asked_for)
+    for counted_p in counted:
+        product_resp[counted_p] = {'times': counted[counted_p], 'percent': counted[counted_p] / len(all_products_asked_for)}
+    return product_resp
+
 
 ## MAIN ##
 session_dic = {}
